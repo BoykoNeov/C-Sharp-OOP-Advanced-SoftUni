fb6e850f0b3702481d07cf4aa0844ffa9e62297f
BoykoNeov <boikoneov@gmail.com>
2018-04-04 17:09:01 +0300

    :fallen_leaf: work on "reflection and attributes" exc - task 2 completed

---
+++ ReflectionNAttributes/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Reflection;
+    using System.Linq;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            string input = string.Empty;
+            ConstructorInfo blackBoxIntConstructor = (typeof(BlackBoxInteger).GetConstructor(BindingFlags.NonPublic|BindingFlags.Instance, null, new Type[] { typeof(int) }, null));
+
+            BlackBoxInteger blackBoxInteger = blackBoxIntConstructor.Invoke(new object[] {0}) as BlackBoxInteger;
+            MethodInfo[] blackBoxIntMethods = blackBoxInteger.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputs = input.Split(new char[] { '_' });
+                FieldInfo[] blackBoxFields = blackBoxInteger.GetType().GetFields();
+                string methodName = inputs[0];
+                int parameter = int.Parse(inputs[1]);
+
+                MethodInfo methodToInvoke = blackBoxIntMethods.Where(m => m.Name == methodName).First();
+                methodToInvoke.Invoke(blackBoxInteger, new object[] { parameter });
+
+                FieldInfo innerIntField = blackBoxInteger.GetType().GetField("innerValue", BindingFlags.NonPublic | BindingFlags.Instance);
+                int result = (int)(innerIntField.GetValue(blackBoxInteger));
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
