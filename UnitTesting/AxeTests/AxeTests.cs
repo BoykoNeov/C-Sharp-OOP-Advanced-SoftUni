8ad670e94a3032d0befc46b1d2f36ce4c55b0850
BoykoNeov <boikoneov@gmail.com>
2018-04-07 00:29:58 +0300

    :sandal: completed first part ot unit test lab

--- UnitTesting/AxeTests/AxeTests.cs
+++ UnitTesting/AxeTests/AxeTests.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Tests
 {
     public class AxeTests
     {
+        private const int AxeDurability = 10;
+        private const int AxeAttack = 10;
+        private const int DummyHealth = 10;
+        private const int DummyExperience = 10;
+
+        private Axe axe;
+        private Dummy dummy;
+
+        [SetUp]
+        public void CreateInitial()
+        {
+            this.dummy = new Dummy(DummyHealth, DummyExperience);
+            this.axe = new Axe(AxeAttack, AxeDurability);
+        }
+
         [Test]
         public void AxeLosesDurabilityAfterAttack()
         {
-            Axe axe = new Axe(10,10);
-            Dummy dummy = new Dummy(10, 10);
             axe.Attack(dummy);
             Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
         }
--- UnitTesting/AxeTests/DummyTests.cs
+++ UnitTesting/AxeTests/DummyTests.cs
@@ -1,36 +1,65 @@
-﻿using NUnit.Framework;
+﻿// For successful use of NUnuit the following packages should be install through NuGet:
+// Microsoft.Net.Test.Sdk
+// NUnit
+// Nunit3TestAdapter
+
+using NUnit.Framework;
 using System;
 
 namespace Tests
 {
     public class DummyTests
     {
+        private const int AxeDurability = 10;
+        private const int AxeAttack = 10;
+        private const int DummyHealth = 10;
+        private const int DummyExperience = 10;
+
+        private Axe axe;
+        private Dummy dummy;
+
+        [SetUp]
+        public void CreateInitial()
+        {
+            this.dummy = new Dummy(DummyHealth, DummyExperience);
+            this.axe = new Axe(AxeAttack, AxeDurability);
+        }
+
         [Test]
         public void DummyLosesHealthWhenAttacked()
         {
-            Axe axe = new Axe(2, 10);
-            Dummy dummy = new Dummy(10, 10);
             axe.Attack(dummy);
-            Assert.AreEqual(8, dummy.Health);
+            Assert.AreEqual(DummyHealth - AxeAttack, dummy.Health, "After receiving 10 points of damage, dummy health is not 10 points less");
+        }
+
+        [Test]
+        public void AttackingWithDefaultAttackShouldKillDummy()
+        {
+            axe.Attack(dummy);
+            Assert.That(dummy.IsDead() == true, "Dummy with 10 hitpoints should be dead after being attacked for 10");
         }
 
         [Test]
         public void DeadDummyThrowsExceptionWhenAttacked()
         {
-            Axe axe = new Axe(10, 10);
-            Dummy dummy = new Dummy(10, 10);
-            axe.Attack(dummy);
-            axe.Attack(dummy);
-            // Assert.Throws(Is.TypeOf<InvalidOperationException>().And.Message.EqualTo("Dummy is dead."), delegate {})
-           // Assert.Throws<InvalidOperationException>(delegate { var a = axe.Attack(dummy) }, "Dead dummy does not throw exception if attacked.");
+            dummy.TakeAttack(dummy.Health);
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy), "Attacking dead dummy doesn't throw an Invalid Operation Exception");
+            Assert.That(ex.Message, Is.EqualTo("Dummy is dead."), "Exception message is not 'Dummy is dead'");
         }
 
-    //    Assert.Throws(Is.Typeof<MyException>()
-    //             .And.Message.EqualTo( "message" )
-    //             .And.Property( "MyParam" ).EqualTo( 42 ),
-    //  delegate { throw new MyException( "message", 42 );
-    //} );
+        [Test]
+        public void DeadDummyGivesExperience()
+        {
+            dummy.TakeAttack(dummy.Health);
+            int experience = dummy.GiveExperience();
+            Assert.That(experience == DummyExperience, "Dead dummy does not give standart dummy experience");
+        }
 
-
+        [Test]
+        public void AliveDummyGivesExperience()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience(), "Alive dummy trying to give experience doesn't throw an Invalid Operation Exception");
+            Assert.That(ex.Message, Is.EqualTo("Target is not dead."), "Exception message is not 'Dummy is dead'");
+        }
     }
 }
