ab90ecfbf74d87f88a2352cae0f2164153c06dc2
BoykoNeov <boikoneov@gmail.com>
2018-04-10 15:43:59 +0300

    :electric_plug: implemented bubble sort and tests

---
+++ UnitTestingExc/BubbleSort/BubbleSorter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BubbleSorter
+{
+    public static class BubbleSorter<T> where T:IComparable<T>
+    {
+        public static IEnumerable<T> BubbleSort(IEnumerable<T> collectionToSort)
+        {
+            bool unorderedItemsPresent = true;
+            List<T> listToWorkWith = collectionToSort.ToList();
+
+            while (unorderedItemsPresent)
+            {
+                unorderedItemsPresent = false;
+
+                for (int i = 0; i < listToWorkWith.Count - 1; i++)
+                {
+                    if (listToWorkWith[i].CompareTo(listToWorkWith[i + 1]) == 1)
+                    {
+                        unorderedItemsPresent = true;
+                        T temp = listToWorkWith[i];
+                        listToWorkWith[i] = listToWorkWith[i + 1];
+                        listToWorkWith[i + 1] = temp;
+                    }
+                }
+            }
+
+            return listToWorkWith;
+        }
+    }
+}
---
+++ UnitTestingExc/BubbleSortTests/BubbleSorterTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using BubbleSorter;
+using NUnit.Framework;
+
+namespace BubbleSortTests
+{
+    public class BubbleSorterTester
+    {
+        [Test]
+        public void SorterTest()
+        {
+            int[] testInts = new int[] { 1, 4, 5, -10, 20, 0 };
+            testInts = BubbleSorter<int>.BubbleSort(testInts).ToArray();
+            Assert.That(testInts.SequenceEqual(new int[] { -10, 0, 1, 4, 5, 20 }));
+        }
+
+        [Test]
+        public void SorterTest2()
+        {
+            int[] testInts = new int[] { 100, 1, 4, 5, -10, 20, 0, 1, -100 };
+            testInts = BubbleSorter<int>.BubbleSort(testInts).ToArray();
+            Assert.That(testInts.SequenceEqual(new int[] { -100, -10, 0, 1, 1, 4, 5, 20, 100 }));
+        }
+    }
+}
