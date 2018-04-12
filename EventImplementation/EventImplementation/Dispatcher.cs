4c0c91c0c827d4795261fe091d9d3286ebafd5ab
BoykoNeov <boikoneov@gmail.com>
2018-04-12 11:53:55 +0300

    more work is needed, but no time

---
+++ EventImplementation/EventImplementation/Dispatcher.cs
@@ -0,0 +1,29 @@
+﻿public delegate void NameChangeEventHandler(object sender, NameChangeEventArgs args);
+
+public class Dispatcher
+{
+    private string name;
+
+    public event NameChangeEventHandler NameChange;
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            this.name = value;
+            this.OnNameChange(new NameChangeEventArgs(value));
+        }
+    }
+
+    private void OnNameChange(NameChangeEventArgs args)
+    {
+        if (!string.IsNullOrEmpty(args.Name))
+        {
+            this.NameChange(this, args);
+        }
+    }
+}
---
+++ EventImplementation/EventImplementation/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Dispatcher dispacher = new Dispatcher();
+        Handler handler = new Handler();
+
+        dispacher.NameChange += handler.OnDispatcherNameChange;
+
+        string newName = string.Empty;
+        while ((newName = Console.ReadLine()) != "End")
+        {
+            dispacher.Name = newName;
+        }
+    }
+}
